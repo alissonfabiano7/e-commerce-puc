f8a76219a8ef2ce2d07783c243be6786d61314c0
Alisson Fabiano <alisson@onfly.com.br>
2021-11-27 16:54:01 -0300

    Setting Controller

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -2,10 +2,25 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\BaseController;
 
-class SettingController extends Controller
+class SettingController extends BaseController
 {
-    //
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        $this->setPageTitle('Settings', 'Manage Settings');
+        return view('admin.settings.index');
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function update(Request $request)
+    {
+
+    }
 }

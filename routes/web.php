8bb088ce76c2c18f84fe28b23e2ced727b2418d1
Alisson Fabiano <alisson@onfly.com.br>
2021-11-27 14:15:20 -0300

    Criando rotas de login para admin

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+require 'admin.php';
 
 use Illuminate\Support\Facades\Route;
 
@@ -20,5 +21,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::view('/admin', 'admin.dashboard.index');
 
+Route::group(['prefix'  =>  'admin'], function () {
+
+    Route::get('login', 'Admin\LoginController@showLoginForm')->name('admin.login');
+    Route::post('login', 'Admin\LoginController@login')->name('admin.login.post');
+    Route::get('logout', 'Admin\LoginController@logout')->name('admin.logout');
+
+});

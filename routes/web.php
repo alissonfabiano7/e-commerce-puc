6599b3f5e8fa3b176b48e5955f892f68d2649ac5
Alisson Fabiano <alisson@onfly.com.br>
2021-11-27 16:10:15 -0300

    Admin Backend

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,3 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::group(['prefix'  =>  'admin'], function () {
-
-    Route::get('login', 'Admin\LoginController@showLoginForm')->name('admin.login');
-    Route::post('login', 'Admin\LoginController@login')->name('admin.login.post');
-    Route::get('logout', 'Admin\LoginController@logout')->name('admin.logout');
-
-});

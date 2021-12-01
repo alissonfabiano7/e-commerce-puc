500ed13b3dd364cb9e123e50fd6fb38d54d160b8
Alisson Fabiano <alisson@onfly.com.br>
2021-12-01 20:40:22 -0300

    Mais progresso

--- resources/views/admin/settings/includes/social_links.blade.php
+++ resources/views/admin/settings/includes/social_links.blade.php
@@ -1,48 +1,48 @@
 <div class="tile">
     <form action="{{ route('admin.settings.update') }}" method="POST" role="form">
         @csrf
-        <h3 class="tile-title">Social Links</h3>
+        <h3 class="tile-title">Links de redes sociais</h3>
         <hr>
         <div class="tile-body">
             <div class="form-group">
-                <label class="control-label" for="social_facebook">Facebook Profile</label>
+                <label class="control-label" for="social_facebook">Perfil do Facebook</label>
                 <input
                     class="form-control"
                     type="text"
-                    placeholder="Enter facebook profile link"
+                    placeholder="Insira o link do seu perfil do Facebook"
                     id="social_facebook"
                     name="social_facebook"
                     value="{{ config('settings.social_facebook') }}"
                 />
             </div>
             <div class="form-group">
-                <label class="control-label" for="social_twitter">Twitter Profile</label>
+                <label class="control-label" for="social_twitter">Perfil do Twitter</label>
                 <input
                     class="form-control"
                     type="text"
-                    placeholder="Enter twitter profile link"
+                    placeholder="Insira o link do seu perfil do Twitter"
                     id="social_twitter"
                     name="social_twitter"
                     value="{{ config('settings.social_twitter') }}"
                 />
             </div>
             <div class="form-group">
-                <label class="control-label" for="social_instagram">Instagram Profile</label>
+                <label class="control-label" for="social_instagram">Perfil do Instagram</label>
                 <input
                     class="form-control"
                     type="text"
-                    placeholder="Enter instagram profile link"
+                    placeholder="Insira o link do seu perfil do Instagram"
                     id="social_instagram"
                     name="social_instagram"
                     value="{{ config('settings.social_instagram') }}"
                 />
             </div>
             <div class="form-group">
-                <label class="control-label" for="social_linkedin">LinkedIn Profile</label>
+                <label class="control-label" for="social_linkedin">Perfil do LinkedIn</label>
                 <input
                     class="form-control"
                     type="text"
-                    placeholder="Enter linkedin profile link"
+                    placeholder="Insira o link do seu perfil do LinkedIn"
                     id="social_linkedin"
                     name="social_linkedin"
                     value="{{ config('settings.social_linkedin') }}"
@@ -52,7 +52,7 @@
         <div class="tile-footer">
             <div class="row d-print-none mt-2">
                 <div class="col-12 text-right">
-                    <button class="btn btn-success" type="submit"><i class="fa fa-fw fa-lg fa-check-circle"></i>Update Settings</button>
+                    <button class="btn btn-success" type="submit"><i class="fa fa-fw fa-lg fa-check-circle"></i>Atualizar Configurações</button>
                 </div>
             </div>
         </div>

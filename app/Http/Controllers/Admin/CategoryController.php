59a3553f4ef5f60d6231247f9ba690a37b6e817b
Alisson Fabiano <alisson@onfly.com.br>
2021-12-01 23:12:12 -0300

    CRUD de Categorias

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use App\Contracts\CategoryContract;
 use App\Http\Controllers\BaseController;
 
 /**
@@ -11,5 +12,114 @@ use App\Http\Controllers\BaseController;
  */
 class CategoryController extends BaseController
 {
-    //
+    /**
+     * @var CategoryContract
+     */
+    protected $categoryRepository;
+
+    /**
+     * CategoryController constructor.
+     * @param CategoryContract $categoryRepository
+     */
+    public function __construct(CategoryContract $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        $categories = $this->categoryRepository->listCategories();
+
+        $this->setPageTitle('Categorias', 'Lista de todas as Categorias');
+        return view('admin.categories.index', compact('categories'));
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function create()
+    {
+        $categories = $this->categoryRepository->listCategories('id', 'asc');
+
+        $this->setPageTitle('Categorias', 'Criar Categoria');
+        return view('admin.categories.create', compact('categories'));
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name'      =>  'required|max:191',
+            'parent_id' =>  'required|not_in:0',
+            'image'     =>  'mimes:jpg,jpeg,png|max:1000'
+        ]);
+
+        $params = $request->except('_token');
+
+        $category = $this->categoryRepository->createCategory($params);
+
+        if (!$category) {
+            return $this->responseRedirectBack('Um erro ocorreu enquanto se criava a categoria', 'error', true, true);
+        }
+        return $this->responseRedirect('admin.categories.index', 'Categoria criada com sucesso' ,'success',false, false);
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        $targetCategory = $this->categoryRepository->findCategoryById($id);
+        $categories = $this->categoryRepository->listCategories();
+
+        $this->setPageTitle('Categorias', 'Editar Categoria : '.$targetCategory->name);
+        return view('admin.categories.edit', compact('categories', 'targetCategory'));
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function update(Request $request)
+    {
+        $this->validate($request, [
+            'name'      =>  'required|max:191',
+            'parent_id' =>  'required|not_in:0',
+            'image'     =>  'mimes:jpg,jpeg,png|max:1000'
+        ]);
+
+        $params = $request->except('_token');
+
+        $category = $this->categoryRepository->updateCategory($params);
+
+        if (!$category) {
+            return $this->responseRedirectBack('Um erro ocorreu enquanto se atualizava a categoria', 'error', true, true);
+        }
+        return $this->responseRedirectBack('Categoria atualizada com sucesso' ,'success',false, false);
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function delete($id)
+    {
+        $category = $this->categoryRepository->deleteCategory($id);
+
+        if (!$category) {
+            return $this->responseRedirectBack('Um erro ocorreu enquanto se excluía a categoria', 'error', true, true);
+        }
+        return $this->responseRedirect('admin.categories.index', 'Categoria excluída com sucesso' ,'success',false, false);
+    }
+
+
 }

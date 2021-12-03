d2ba6f6b3c20586418cb27ec3d72485cdfb2fce2
Alisson Fabiano <alisson@onfly.com.br>
2021-12-03 15:06:19 -0300

    CRUD de atributos

---
+++ app/Http/Controllers/Admin/AttributeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\BaseController;
+use App\Contracts\AttributeContract;
+
+class AttributeController extends BaseController
+{
+    protected $attributeRepository;
+
+    public function __construct(AttributeContract $attributeRepository)
+    {
+        $this->attributeRepository = $attributeRepository;
+    }
+
+    public function index()
+    {
+        $attributes = $this->attributeRepository->listAttributes();
+
+        $this->setPageTitle('Atributos', 'Listar todos os atributos');
+        return view('admin.attributes.index', compact('attributes'));
+    }
+
+    public function create()
+    {
+        $this->setPageTitle('Atributos', 'Criar atributos');
+        return view('admin.attributes.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'code'          =>  'required',
+            'name'          =>  'required',
+            'frontend_type' =>  'required'
+        ]);
+
+        $params = $request->except('_token');
+
+        $attribute = $this->attributeRepository->createAttribute($params);
+
+        if (!$attribute) {
+            return $this->responseRedirectBack('Um erro ocorreu enquanto se criava um atributo.', 'error', true, true);
+        }
+        return $this->responseRedirect('admin.attributes.index', 'Atributo criado com sucesso' ,'success',false, false);
+    }
+
+    public function edit($id)
+    {
+        $attribute = $this->attributeRepository->findAttributeById($id);
+
+        $this->setPageTitle('Atributos', 'Editar atributos : '.$attribute->name);
+        return view('admin.attributes.edit', compact('attribute'));
+    }
+
+    public function update(Request $request)
+    {
+        $this->validate($request, [
+            'code'          =>  'required',
+            'name'          =>  'required',
+            'frontend_type' =>  'required'
+        ]);
+
+        $params = $request->except('_token');
+
+        $attribute = $this->attributeRepository->updateAttribute($params);
+
+        if (!$attribute) {
+            return $this->responseRedirectBack('Um erro ocorreu enquanto se atualizava um atributo.', 'error', true, true);
+        }
+        return $this->responseRedirectBack('Atributo atualizado com sucesso' ,'success',false, false);
+    }
+
+    public function delete($id)
+    {
+        $attribute = $this->attributeRepository->deleteAttribute($id);
+
+        if (!$attribute) {
+            return $this->responseRedirectBack('Um erro ocorreu enquanto se excluía um atributo', 'error', true, true);
+        }
+        return $this->responseRedirect('admin.attributes.index', 'Atributo excluído com sucesso' ,'success',false, false);
+    }
+}

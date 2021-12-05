e15179eb3acc56bf17d29e066223117c0f69fd9f
Alisson Fabiano <alisson@onfly.com.br>
2021-12-04 22:49:55 -0300

    Views para o frontend

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,96 @@
+@extends('site.app')
+@section('title', 'Register')
+@section('content')
+    <section class="section-pagetop bg-dark">
+        <div class="container clearfix">
+            <h2 class="title-page">Registrar</h2>
+        </div>
+    </section>
+    <section class="section-content bg padding-y">
+        <div class="container">
+            <div class="col-md-6 mx-auto">
+                <div class="card">
+                    <header class="card-header">
+                        <h4 class="card-title mt-2">Entrar</h4>
+                    </header>
+                    <article class="card-body">
+                        <form action="{{ route('register') }}" method="POST" role="form">
+                            @csrf
+                            <div class="form-row">
+                                <div class="col form-group">
+                                    <label for="first_name">Primeiro nome</label>
+                                    <input type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" id="first_name" value="{{ old('first_name') }}">
+                                    @error('first_name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                                <div class="col form-group">
+                                    <label for="last_name">Último nome</label>
+                                    <input type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" id="last_name" value="{{ old('last_name') }}">
+                                    @error('last_name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Endereço de E-Mail</label>
+                                <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email" value="{{ old('email') }}">
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Senha</label>
+                                <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password">
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="password_confirmation">Confirme a senha</label>
+                                <input type="password" class="form-control @error('password_confirmation') is-invalid @enderror" name="password_confirmation" id="password_confirmation">
+                                @error('password_confirmation')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="address">Endereço</label>
+                                <input class="form-control" type="text" name="address" id="address" value="{{ old('address') }}">
+                            </div>
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="city">Cidade</label>
+                                    <input type="text" class="form-control" name="city" id="city" value="{{ old('city') }}">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="country">Estado</label>
+                                    <select id="country" class="form-control" name="country">
+                                        <option> Selecione...</option>
+                                        <option value="United Kingdom">United Kingdom</option>
+                                        <option value="France">France</option>
+                                        <option value="United States" selected="">United States</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-success btn-block"> Entrar </button>
+                            </div>
+                            <small class="text-muted">Ao clicar em entrar, você assume que está de acordo com nossas <br>Políticas e termos de uso </small>
+                        </form>
+                    </article>
+                    <div class="border-top card-body text-center">Tem uma conta <a href="{{ route('login') }}">Entre</a></div>
+                </div>
+            </div>
+        </div>
+    </section>
+@stop
---
+++ resources/views/site/partials/footer.blade.php
@@ -0,0 +1,75 @@
+<!-- ========================= FOOTER ========================= -->
+<footer class="section-footer bg-dark white">
+    <div class="container">
+        <section class="footer-top padding-top">
+            <div class="row">
+                <aside class="col-sm-3 col-md-3 white">
+                    <h5 class="title">Serviços para o cliente</h5>
+                    <ul class="list-unstyled">
+                        <li> <a href="#">Centro de ajuda</a></li>
+                        <li> <a href="#">Reembolsos</a></li>
+                        <li> <a href="#">Termos e políticas</a></li>
+                    </ul>
+                </aside>
+                <aside class="col-sm-3  col-md-3 white">
+                    <h5 class="title">Minha conta</h5>
+                    <ul class="list-unstyled">
+                        <li> <a href="#"> Login </a></li>
+                        <li> <a href="#"> Registre-se </a></li>
+                        <li> <a href="#"> Configurações da Conta </a></li>
+                        <li> <a href="#"> Minhas ordens </a></li>
+                        <li> <a href="#"> Minha lista de desejos </a></li>
+                    </ul>
+                </aside>
+                <aside class="col-sm-3  col-md-3 white">
+                    <h5 class="title">About</h5>
+                    <ul class="list-unstyled">
+                        <li> <a href="#"> Nossa história </a></li>
+                        <li> <a href="#"> Como comprar </a></li>
+                        <li> <a href="#"> Entrega e pagamento </a></li>
+                        <li> <a href="#"> Propagandas </a></li>
+                        <li> <a href="#"> Parceria </a></li>
+                    </ul>
+                </aside>
+                <aside class="col-sm-3">
+                    <article class="white">
+                        <h5 class="title">Contatos</h5>
+                        <p>
+                            <strong>Telefone: </strong> +55(31)994178583
+                            <br>
+                            <strong>E-mail: </strong> alissonfabiano7@gmail.com
+                        </p>
+
+                        <div class="btn-group white">
+                            <a class="btn btn-facebook" title="Facebook" target="_blank" href="#"><i
+                                    class="fab fa-facebook-f  fa-fw"></i></a>
+                            <a class="btn btn-instagram" title="Instagram" target="_blank" href="#"><i
+                                    class="fab fa-instagram  fa-fw"></i></a>
+                            <a class="btn btn-youtube" title="Youtube" target="_blank" href="#"><i
+                                    class="fab fa-youtube  fa-fw"></i></a>
+                            <a class="btn btn-twitter" title="Twitter" target="_blank" href="#"><i
+                                    class="fab fa-twitter  fa-fw"></i></a>
+                        </div>
+                    </article>
+                </aside>
+            </div>
+            <!-- row.// -->
+            <br>
+        </section>
+        <section class="footer-bottom row border-top-white">
+            <div class="col-sm-6">
+                <p class="text-white-50"> Feito por Alisson Fabiano</p>
+            </div>
+            <div class="col-sm-6">
+                <p class="text-md-right text-white-50">
+                    Copyright &copy
+                    <br>
+                    <a href="http://bootstrap-ecommerce.com" class="text-white-50">Bootstrap-ecommerce UI kit</a>
+                </p>
+            </div>
+        </section>
+        <!-- //footer-top -->
+    </div>
+    <!-- //container -->
+</footer>
+<!-- ========================= FOOTER END // ========================= -->

3d9bccbafb0cea79ea7a5510f172a8ec00f3ada2
Alisson Fabiano <alisson@onfly.com.br>
2021-12-07 16:43:00 -0300

    Finalizando o carrinho de compras

---
+++ resources/views/site/pages/cart.blade.php
@@ -0,0 +1,94 @@
+@extends('site.app')
+@section('title', 'Shopping Cart')
+@section('content')
+    <section class="section-pagetop bg-dark">
+        <div class="container clearfix">
+            <h2 class="title-page">Carrinho</h2>
+        </div>
+    </section>
+    <section class="section-content bg padding-y border-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    @if (Session::has('message'))
+                        <p class="alert alert-success">{{ Session::get('message') }}</p>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <main class="col-sm-9">
+                    @if (\Cart::isEmpty())
+                        <p class="alert alert-warning">Seu carrinho está vazio.</p>
+                    @else
+                        <div class="card">
+                            <table class="table table-hover shopping-cart-wrap">
+                                <thead class="text-muted">
+                                <tr>
+                                    <th scope="col">Product</th>
+                                    <th scope="col" width="120">Quantidade</th>
+                                    <th scope="col" width="120">Preço</th>
+                                    <th scope="col" class="text-right" width="200">Ação</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach(\Cart::getContent() as $item)
+                                    <tr>
+                                        <td>
+                                            <figure class="media">
+                                                <figcaption class="media-body">
+                                                    <h6 class="title text-truncate">{{ Str::words($item->name,20) }}</h6>
+                                                    @foreach($item->attributes as $key  => $value)
+                                                        <dl class="dlist-inline small">
+                                                            <dt>{{ ucwords($key) }}: </dt>
+                                                            <dd>{{ ucwords($value) }}</dd>
+                                                        </dl>
+                                                    @endforeach
+                                                </figcaption>
+                                            </figure>
+                                        </td>
+                                        <td>
+                                            <var class="price">{{ $item->quantity }}</var>
+                                        </td>
+                                        <td>
+                                            <div class="price-wrap">
+                                                <var class="price">{{ config('settings.currency_symbol'). $item->price }}</var>
+                                                <small class="text-muted">cada</small>
+                                            </div>
+                                        </td>
+                                        <td class="text-right">
+                                            <a href="{{ route('checkout.cart.remove', $item->id) }}" class="btn btn-outline-danger"><i class="fa fa-times"></i> </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    @endif
+                </main>
+                <aside class="col-sm-3">
+                    <a href="{{ route('checkout.cart.clear') }}" class="btn btn-danger btn-block mb-4">Clear Cart</a>
+                    <p class="alert alert-success">Adicione 5 reais de itens elegíveis para sua ordem para se qualificar para o frete grátis. </p>
+                    <dl class="dlist-align h4">
+                        <dt>Total:</dt>
+                        <dd class="text-right"><strong>{{ config('settings.currency_symbol') }}{{ \Cart::getSubTotal() }}</strong></dd>
+                    </dl>
+                    <hr>
+                    <figure class="itemside mb-3">
+                        <aside class="aside"><img src="{{ asset('frontend/images/icons/pay-visa.png') }}"></aside>
+                        <div class="text-wrap small text-muted">
+                            Pague 50
+                        </div>
+                    </figure>
+                    <figure class="itemside mb-3">
+                        <aside class="aside"> <img src="{{ asset('frontend/images/icons/pay-mastercard.png') }}"> </aside>
+                        <div class="text-wrap small text-muted">
+                            Pague com MasterCard e economize 40%
+                            <br> Blá blá blá
+                        </div>
+                    </figure>
+                    <a href="#" class="btn btn-success btn-lg btn-block">Prosseguir para o checkout</a>
+                </aside>
+            </div>
+        </div>
+    </section>
+@stop

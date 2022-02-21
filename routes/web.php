2ae70cb0087c06e31650beac7e58f6092e4df451
Alisson Fabiano <alisson@onfly.com.br>
2022-02-21 17:46:40 -0300

    Correção de linguagem

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
-require 'admin.php';
-
-use Illuminate\Support\Facades\Route;
 
 header('Access-Control-Allow-Origin:  *');
 header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, PATCH, DELETE');
 header('Access-Control-Allow-Headers: Accept, Content-Type, X-Auth-Token, Origin, Authorization');
+
+require 'admin.php';
+
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
